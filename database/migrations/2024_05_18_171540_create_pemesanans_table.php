76c5ec507260baa9186083460047ad3207d53c8b
zamyreyn-code <arieyzamimy@gmail.com>
2024-05-19 00:21:31 +0700

    Initial commit

---
+++ database/migrations/2024_05_18_171540_create_pemesanans_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pemesanans', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('makanan');
+            $table->integer('harga');
+            $table->date('tanggal');
+            $table->integer('jumlah');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pemesanans');
+    }
+};
